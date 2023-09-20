38ccfc98590deee9c7939a888f1ff96e3a490a3d
cueltschey <68715119+cueltschey@users.noreply.github.com>
2023-09-20 11:56:21 -0500

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,34 @@
 
-
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+interface Props{
+  index : number;
+  artists : string[];
+  songs : [string[]][];
 }
 
-export default Home
+const Main = ({index, artists, songs}:Props) => {
+  if(songs.length < 1){
+    return(
+    <h2 className="pleaseInput">Please Input Songs...</h2>
+    )
+  }
+    return(
+      <ul className="list-group" id="songlist">
+        <h1> {artists[index]}</h1>
+        {songs[index].map((song , index, songsArray) => (
+          <li className="list-group-item" id="songdisplay">
+            <div className="row content-align-center">
+              <div className="col">
+                <h4 id="songtitle">{songsArray[index][1]}</h4>
+              </div>
+              <div className="col">
+                <audio controls id="player" src={songsArray[index][0]}></audio>
+              </div>
+            </div>
+          </li>
+        ))}
+
+      </ul>
+    )
+  }
+
+export default Main
